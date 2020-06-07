e07d2a654a46875a7ccdee7af72db20a76944325
Simon Podlipsky <simon@podlipsky.net>
2020-06-07 15:53:57 +0200

    myclabs/php-enum marked as unused

---
+++ src/Enum.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimPod\Bugs;
+
+/**
+ * @template T
+ * @extends \MyCLabs\Enum\Enum<T>
+ * @psalm-immutable
+ */
+abstract class Enum extends \MyCLabs\Enum\Enum
+{
+}
